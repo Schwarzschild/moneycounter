bbd91309242fddda4c9eacb51592706ff4b232f1
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-12-27 19:00:26 -0500

    Organized PnLTests.

--- tests/pnl_tests.py
+++ tests/pnl_tests.py
@@ -7,7 +7,18 @@ class PnLTests(TradesBaseTest):
 
     def test_pnl(self):
         year = 2023
-        df, dt = self.get_df(year, a='ACCNT1', t='TICKER3')
-        realized, unrealized, total = pnl(df, price=298)
-        self.assertAlmostEqual(realized, -1942.0)
-        self.assertAlmostEqual(unrealized, 2347.0)
+
+
+        price = 305
+        for a, t, expected_total, expected_realized in [['ACCNT1', 'TICKER4', 460, 10],
+                                                        ['ACCNT1', 'TICKER1', 650, 700],
+                                                        ['ACCNT2', 'TICKER1', 800, 800],
+                                                        ['ACCNT2', 'TICKER2', 185, 207],
+                                                        ['ACCNT1', 'TICKER3', -187, 199]]:
+            expected_unrealized = expected_total - expected_realized
+            df, dt = self.get_df(year, a=a, t=t)
+            realized, unrealized, total = pnl(df, price=price)
+
+            self.assertAlmostEqual(realized, expected_realized)
+            self.assertAlmostEqual(unrealized, expected_unrealized)
+            self.assertAlmostEqual(total, expected_total)
