dfb469dddb77fe5e136232a54ffacae11e4d609a
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-12-28 09:25:11 -0500

    Added another test.

--- tests/pnl_tests.py
+++ tests/pnl_tests.py
@@ -14,7 +14,9 @@ class PnLTests(TradesBaseTest):
                                                         ['ACCNT1', 'TICKER1', 650, 700],
                                                         ['ACCNT2', 'TICKER1', 800, 800],
                                                         ['ACCNT2', 'TICKER2', 185, 207],
-                                                        ['ACCNT1', 'TICKER3', -187, -199]]:
+                                                        ['ACCNT1', 'TICKER3', -187, -199],
+                                                        ['ACCNT1', 'TICKER5', -330.0, -330.0]]:
+
             expected_unrealized = expected_total - expected_realized
             df, dt = self.get_df(year, a=a, t=t)
             realized, unrealized, total = pnl(df, price=price)
